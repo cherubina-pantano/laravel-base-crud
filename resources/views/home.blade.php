bc37041e63f9e9c22d6b9e5a7e1712a23ab678a2
cherubina-pantano <cherubina.pantano@gmail.com>
2021-01-21 15:48:03 +0100

    Add setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+
+@extends('layouts.main')
+
+@section('content')
+
+    <div class="container mb-5">
+        <h1>Shool HOME Page</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Temporibus reiciendis corporis magnam numquam ipsa earum. Illo perspiciatis asperiores qui quia? 
+        Esse soluta et facere reprehenderit. Voluptatum et dolor illo? Delectus!
+        </p>
+    </div>
+
+@endsection
